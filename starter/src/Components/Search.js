2efe364d27cf79a4f1ce59ae4a77e9a45adc267c
mariam samy makram <mariam.samy.mrmr@gmail.com>
2022-10-23 17:42:54 +0200

    style: Adding handle search action in function and add docs for it.
    Adding handle search action in function and add docs for it.

--- starter/src/Components/Search.js
+++ starter/src/Components/Search.js
@@ -4,6 +4,19 @@ import { search } from "../BooksAPI";
 import Book from "./Book";
 const Search = () => {
   const [books, setBooks] = useState([]);
+  /**
+   * @description Handle submit search form by loading books with query.
+   * @param {*} e 
+   */
+  const handleLoadBooks = (e) => {
+    if (e.target.value) {
+      search(e.target.value, 10).then((data) => {
+        setBooks(data && !data.error ? data : []);
+      });
+    }else{
+      setBooks([])
+    }
+  };
   return (
     <div className="search-books">
       <div className="search-books-bar">
@@ -14,11 +27,7 @@ const Search = () => {
           <input
             type="text"
             placeholder="Search by title, author, or ISBN"
-            onChange={(e) => {
-              search(e.target.value, 10).then((data) => {
-                setBooks(!data.error ? data : []);
-              });
-            }}
+            onChange={handleLoadBooks}
           />
         </div>
       </div>
